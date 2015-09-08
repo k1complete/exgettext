15c1d74a41ec2345357130ba08c77eee0866360b
k1complete <oda@maqcsa.net>
2015-09-08 22:08:46 +0900

    elixir-1.1.0-beta

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exgettext.Mixfile do
   def project do
     [app: :exgettext,
      version: "0.1.0",
-     elixir: "~> 1.1.0-dev or ~> 1.0.0 or ~> 0.15.0-dev",
+     elixir: "~> 1.1.0-beta or ~> 1.0.0 or ~> 0.15.0-dev",
      compilers: Mix.compilers ++ [:po],
      description: "Localization package using GNU gettext",
      package: [
