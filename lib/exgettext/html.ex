b635e8667461e9b49df9d8ecc438051b72a7c573
k1complete <oda@maqcsa.net>
2015-09-21 10:01:44 +0900

    htmlfilter for ExDoc

---
+++ lib/exgettext/html.ex
@@ -0,0 +1,19 @@
+defmodule Exgettext.HTML do
+  import Exgettext.Runtime, only: [gettext: 2]
+  def run(module_nodes, config) when is_map(config) do
+    m = module_nodes |> 
+      Enum.map(fn(mod) ->
+                 app = Exgettext.Util.get_app(mod.module)
+                 Map.update(mod, :moduledoc, nil, &(gettext(app, &1))) |>
+                 Map.update(ret, :docs, nil, 
+                            &(Enum.map(&1, 
+                                       fn(x) -> 
+                                         Map.update(x, :doc, nil,
+                                                    fn(y) -> 
+                                                      gettext(app, y)
+                                                    end)
+                                       end)))
+               end)
+    ExDoc.Formatter.HTML.run(m, config)
+  end
+end
