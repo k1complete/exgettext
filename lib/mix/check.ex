098e966c1d2cb3a2d005b83e6fecd60906290fd3
k1complete <oda@maqcsa.net>
2016-02-28 21:46:18 +0900

    add l10n.check command

---
+++ lib/mix/check.ex
@@ -0,0 +1,53 @@
+defmodule Mix.Tasks.L10n.Check do
+  use Mix.Task
+  @shortdoc "run msgfmt -v --statistics -c for checking po-files"
+  @moduledoc """
+  check portable object files for current project.
+
+  ## Synopsis
+
+  ```
+      mix l10n.check [--list]
+  ```
+
+  ## Arguments
+ 
+    * --listt  -- add fuzzy, and untransated file list
+
+  ## Environment
+  
+    * LANG -- localize target language for `Language`
+
+  ## Mix Environment
+
+    * project[:app] -- basename for portable object file.
+
+  ## Files
+
+    * priv/po/**/*.po -- target po-files
+
+  ### Input
+
+
+  ### Output
+
+
+  """
+  def run(opt) do
+    {opt, _args, _rest} = OptionParser.parse(opt)
+    env  = Keyword.get(opt, :locale, System.get_env("LANG"))
+    lang = Exgettext.Runtime.getlang(env)
+    app = Mix.Project.config[:app]
+    pofiles = Exgettext.Util.pofiles(lang)
+    Mix.shell.info("check #{pofiles}")
+    IO.inspect [pofiles: pofiles]
+    Enum.reduce(pofiles, 0,
+                fn(x, a) ->
+                  IO.inspect [x: x, a: a]
+                  cmd = "msgfmt -c -v --statistics -o /dev/null #{opt} #{x}"
+                  IO.inspect [cmd: cmd]
+                  Mix.shell.info(cmd)
+                  a+Mix.shell.cmd(cmd)
+                end)
+  end
+end
