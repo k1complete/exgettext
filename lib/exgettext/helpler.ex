6d0239f31ab44b23709f0912e0633cae39203dfa
k1complete <oda@maqcsa.net>
2019-05-31 20:58:37 +0900

    introspection fix

--- lib/exgettext/helpler.ex
+++ lib/exgettext/helpler.ex
@@ -6,7 +6,6 @@ defmodule Exgettext.Helper do
   """
   def h() do
     Exgettext.Introspection.h(IEx.Helpers)
-    dont_display_result()
   end
   @h_modules [__MODULE__, IEx.Helpers, Kernel, Kernel.SpecialForms]
 
@@ -15,38 +14,9 @@ defmodule Exgettext.Helper do
   for the given function/arity pair.
   if there is not localized document, prints original documentation.
   """
-  defmacro h({:/, _, [call, arity]} = other) do
-    args =
-      case Macro.decompose_call(call) do
-        {_mod, :__info__, []} when arity == 1 ->
-          [Module, :__info__, 1]
-        {mod, fun, []} ->
-          [mod, fun, arity]
-        {fun, []} ->
-          [@h_modules, fun, arity]
-        _ ->
-          [other]
-      end
-
+  defmacro h(term) do
     quote do
-      Exgettext.Introspection.h(unquote_splicing(args))
-    end
-  end
-  defmacro h(call) do
-    args =
-      case Macro.decompose_call(call) do
-        {_mod, :__info__, []} ->
-          [Module, :__info__, 1]
-        {mod, fun, []} ->
-          [mod, fun]
-        {fun, []} ->
-          [@h_modules, fun]
-        _ ->
-          [call]
-      end
-
-    quote do
-      Exgettext.Introspection.h(unquote_splicing(args))
+      Exgettext.Introspection.h(unquote(IEx.Introspection.decompose(term, __CALLER__)))
     end
   end
 
